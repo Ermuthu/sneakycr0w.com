049c168eb4ac771e5dbf4ea6ce70da3ad5551ba1
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2017-01-01 09:22:18 -0800

    removed Ada model

--- static_pages/models.py
+++ static_pages/models.py
@@ -2,13 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Ada(models.Model):
-    image = models.ImageField(upload_to='uploads/crow')
-    github = models.CharField(max_length=500)
-    twitter = models.CharField(max_length=500)
-    def __str__(self):
-        return "Ada"
-
 class Project(models.Model):
     title = models.CharField(max_length=100)
     desc = models.TextField(max_length=144)
