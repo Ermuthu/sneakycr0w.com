e8fe2bf4872c35bbd3e190776d8d6c129a05bc40
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2017-01-08 12:52:47 -0800

    adjusted models and css

--- static_pages/models.py
+++ static_pages/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     desc = models.TextField(max_length=144)
-    image = models.ImageField(upload_to='uploads/projects', blank=True, null=True)
+    image = models.ImageField(upload_to='/uploads/projects', blank=True, null=True)
     image_url = models.URLField()
     def __str__(self):
         return self.title
