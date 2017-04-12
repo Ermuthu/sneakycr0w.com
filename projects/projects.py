261c3a668a4c416caadb399a7aa9e40d9c63daa0
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2017-04-12 00:40:30 -0700

    added new ghostchat project

--- projects/projects.py
+++ projects/projects.py
@@ -1,19 +1,23 @@
 # Projects.py - A flask app for displaying current projects
-# TODO: Create CRUD buttons
 # TODO: Create MongoDB connection
+# TODO: Create CRUD buttons
 # A py by Zachary E. Sohovich
 
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
 from flask.ext.pymongo import PyMongo
-
 
 app = Flask(__name__)
 mongo = PyMongo(app)
 
-@app.route('/')
+@app.route('/', METHODS=['GET'])
 def projects():
+    # Return current collection/document of projects
     return render_template('index.html')
+
+@app.route('/add', METHODS=['POST'])
+def add_project():
+    # Return form for adding a new project
+    return redirect('/')
 
 if __name__=="__main__":
     app.run()
