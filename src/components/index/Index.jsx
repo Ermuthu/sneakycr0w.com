0830e60e71c91242fee5a0d95bff404bf4c7a9ab
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2017-11-08 15:17:31 -0800

    removed 'exact' prop as required proptype

--- src/components/index/Index.jsx
+++ src/components/index/Index.jsx
@@ -5,25 +5,23 @@ import { Route, Switch } from 'react-router-dom';
 import NavigationDrawer from 'react-md/lib/NavigationDrawers';
 import NavLink from './NavLink';
 
-import Home from '../portfolio/Portfolio.jsx';
+import Portfolio from '../portfolio/Portfolio.jsx';
 import About from '../about/About.jsx';
 import Contact from '../contact/Contact.jsx';
 
 const navItems = [
   {
     exact: true,
-    label: Home,
+    label: 'Portfolio',
     to: '/',
     icon: 'home',
   },
   {
-    exact: false,
     label: 'About',
     to: '/about',
     icon: 'bookmark',
   },
   {
-    exact: false,
     label: 'Contact',
     to: '/contact',
     icon: 'donut_large',
@@ -39,7 +37,7 @@ const Index = () => (
         navItems={navItems.map(props => <NavLink {...props} key={props.to} />)}
       >
         <Switch key={location.key}>
-          <Route exact path="/" location={location} component={Home} />
+          <Route exact path="/" location={location} component={Portfolio} />
           <Route path="/about" location={location} component={About} />
           <Route path="/contact" location={location} component={Contact} />
         </Switch>
--- src/components/index/NavLink.js
+++ src/components/index/NavLink.js
@@ -19,7 +19,7 @@ const NavLink = ({ label, to, exact, icon }) => (
 NavLink.propTypes = {
   label: propTypes.String.isRequired,
   to: propTypes.String.isRequired,
-  exact: propTypes.Boolean.isRequired,
+  exact: propTypes.Boolean,
   icon: propTypes.String.isRequired,
 };
 
