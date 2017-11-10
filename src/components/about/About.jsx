b617d12490104d2d9221c5cab42f6f3f707f8d27
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2017-11-10 13:58:36 -0800

    sneakycr0w with exclusivity to front-end

--- src/components/about/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const About = () => (
-  <div>
-    <h2>&#60;Hello World &#47;&#62;</h2>
-  </div>
-);
-
-export default About;
--- src/components/contact/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Contact = () => (
-  <div>
-    <h2>Contact component works!</h2>
-  </div>
-);
-
-export default Contact;
