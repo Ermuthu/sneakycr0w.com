2386525397b63749e625a18c12819f6c4c08cf76
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2017-11-10 17:18:08 -0800

    added axios request to get list of repos from my github

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react";
+import axios from "axios";
+
 import logo from "./logo.svg";
 import { Container, Header, Main, Logo, NavItem } from "./styled-utils";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      repos: []
+    };
+  }
+  componentDidMount() {
+    axios.get("https://api.github.com/users/sneakycr0w/repos").then(res => {
+      this.setState({ repos: res.data });
+      console.log(this.state);
+      return res.data;
+    });
+  }
   render() {
     return (
       <Container>
