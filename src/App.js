d2709a8e4a919b087f931d6da918307815ff6cca
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2017-11-11 11:46:22 -0800

    rewrote App.js as a stateless function since it doesnt utilize state anymore

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import Home from './Home';
 
 import logo from './logo.svg';
 import { Container, Navigation, NavItem, Header, Logo } from './styled-utils';
 
-class App extends Component {
-  render() {
-    return (
-      <Container>
-        <Navigation>
-          <NavItem href="/home">Home</NavItem>
-          <NavItem href="/about">About</NavItem>
-          <NavItem href="/contact">Contact</NavItem>
-        </Navigation>
-        <Header>
-          <Logo src={logo} alt="Atomic Crow" />
-        </Header>
-
-        <Home />
-      </Container>
-    );
-  }
-}
+const App = () => (
+  <Container>
+    <Navigation>
+      <NavItem href="/home">Home</NavItem>
+      <NavItem href="/about">About</NavItem>
+      <NavItem href="/contact">Contact</NavItem>
+    </Navigation>
+    <Header>
+      <Logo src={logo} alt="Atomic Crow" />
+    </Header>
+    <Home />
+  </Container>
+);
 
 export default App;
