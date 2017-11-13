38772b3b8de3467e7af9041228b18c82e88aece1
Zachary E. Sohovich <zacharyesohovich@gmail.com>
2017-11-12 23:08:50 -0800

    Styled updates

--- src/Profile.js
+++ src/Profile.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import logo from './logo.svg';
 
@@ -7,13 +7,30 @@ const Container = styled.div`
   color: white;
   grid-row-start: 2;
   margin: 5%;
-  a {
-    color: white;
-  }
+  grid-column: span 4;
 `;
 
 const Logo = styled.img`
   height: 75px;
+`;
+
+const Button = styled.a`
+  text-align: center;
+  line-height: 40px;
+  display: inline-block;
+  width: 150px;
+  height: 40px;
+  background-color: #5bba49;
+  border-radius: 2px;
+  margin: 10px;
+  color: black;
+  ${props =>
+    props.primary &&
+    css`
+      background: none;
+      box-shadow: inset 0 0 0 4px #5bba49;
+      color: white;
+    `};
 `;
 
 const Profile = () => (
@@ -23,6 +40,8 @@ const Profile = () => (
     <p>
       <a href="https://twitter.com/sneakycr0w">Twitter</a>
     </p>
+    <Button>Button 1</Button>
+    <Button primary>Button 2</Button>
   </Container>
 );
 
